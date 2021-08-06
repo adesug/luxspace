dfee6d4ef6320a9877bfbebb469e8980f0c838a2
adesug <adesugiantoro89@gmail.com>
2021-08-07 01:31:50 +0700

    setup folder structuring

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    plugins : [
+        require("tailwindscss")("/tailwind.config.js"),
+        require("autoprefixer")
+    ],
+};
--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/css/app.css'
 
 function App() {
   return (
