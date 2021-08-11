b663cc57320aeb1c45bf8daac2d51e8dce5fdbb1
adesug <adesugiantoro89@gmail.com>
2021-08-11 21:58:27 +0700

    add usescrollanchor and usescrollanchor

--- src/pages/HomePages.js
+++ src/pages/HomePages.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Header from 'parts/Header';
-import Hero from 'parts/Hero';
+import Hero from 'parts/HomePage/Hero';
 import JustArrived from 'parts/HomePage/JustArrived';
 import BrowseRoom from 'parts/HomePage/BrowseRoom';
 import Client from 'parts/Client';
 import Sitemap from 'parts/HomePage/Sitemap';
 import Footer from 'parts/Footer';
 
-export default function HomePages() {
+import UseScrollAnchor from 'helpers/hooks/UseScrollAnchor';
+import UseModalDOM from 'helpers/hooks/UseModalDOM';
 
+export default function HomePages() {
+    UseScrollAnchor();
+    UseModalDOM();
     return (
          <>
          <Header theme="white" position="absolute"/>
